624740c2637bdaca381eb37a12f1e2de49976bb9
Alberto Sarco <sarco.jesus@gmail.com>
2016-07-22 13:22:27 -0400

    commit

--- app/src/main/java/com/smartbuilders/smartsales/ecommerceandroidapp/RequestResetUserPasswordFragment.java
+++ app/src/main/java/com/smartbuilders/smartsales/ecommerceandroidapp/RequestResetUserPasswordFragment.java
@@ -6,8 +6,6 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.content.pm.ActivityInfo;
-import android.content.res.Configuration;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
