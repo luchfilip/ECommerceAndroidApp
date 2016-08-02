c37f41067e9295f2b361c229cbeb846457953bd1
Alberto Sarco <sarco.jesus@gmail.com>
2016-08-02 11:19:46 -0400

    commit

--- app/src/main/java/com/smartbuilders/smartsales/ecommerce/data/BannerSectionDB.java
+++ app/src/main/java/com/smartbuilders/smartsales/ecommerce/data/BannerSectionDB.java
@@ -2,6 +2,7 @@ package com.smartbuilders.smartsales.ecommerce.data;
 
 import android.content.Context;
 
+import com.jasgcorp.ids.model.User;
 import com.smartbuilders.smartsales.ecommerce.model.BannerSection;
 
 /**
@@ -10,14 +11,16 @@ import com.smartbuilders.smartsales.ecommerce.model.BannerSection;
 public class BannerSectionDB {
 
     private Context mContext;
+    private User mUser;
 
-    public BannerSectionDB(Context context){
+    public BannerSectionDB(Context context, User user){
         this.mContext = context;
+        this.mUser = user;
     }
 
     public BannerSection getBannerSection() {
         BannerSection bannerSection = new BannerSection();
-        bannerSection.setBanners((new BannerDB(mContext)).getActiveBanners());
+        bannerSection.setBanners((new BannerDB(mContext, mUser)).getActiveBanners());
         return bannerSection;
     }
 }
