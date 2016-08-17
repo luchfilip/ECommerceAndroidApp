8de55aa2a084c80edb7b0649b040082dc2a006ea
Alberto Sarco <sarco.jesus@gmail.com>
2016-08-16 21:18:32 -0400

    commit

--- app/src/main/java/com/smartbuilders/smartsales/ecommerce/data/ProductRecentlySeenDB.java
+++ app/src/main/java/com/smartbuilders/smartsales/ecommerce/data/ProductRecentlySeenDB.java
@@ -32,8 +32,9 @@ public class ProductRecentlySeenDB {
                     "INSERT OR REPLACE INTO PRODUCT_RECENTLY_SEEN (PRODUCT_RECENTLY_SEEN_ID, BUSINESS_PARTNER_ID, " +
                             " USER_ID, PRODUCT_ID, CREATE_TIME, APP_VERSION, APP_USER_NAME, DEVICE_MAC_ADDRESS) " +
                     " VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
-                    new String[]{String.valueOf(UserTableMaxIdDB.getNewIdForTable(mContext, mUser, "PRODUCT_RECENTLY_SEEN")), String.valueOf(businessPartnerId),
-                            String.valueOf(mUser.getServerUserId()), String.valueOf(productId), DateFormat.getCurrentDateTimeSQLFormat(),
+                    new String[]{String.valueOf(UserTableMaxIdDB.getNewIdForTable(mContext, mUser, "PRODUCT_RECENTLY_SEEN")),
+                            String.valueOf(businessPartnerId), String.valueOf(mUser.getServerUserId()),
+                            String.valueOf(productId), DateFormat.getCurrentDateTimeSQLFormat(),
                             Utils.getAppVersionName(mContext), mUser.getUserName(), Utils.getMacAddress(mContext)});
         } catch (Exception e){
             e.printStackTrace();
@@ -42,22 +43,36 @@ public class ProductRecentlySeenDB {
 
     public ArrayList<Product> getProductsRecentlySeenByBusinessPartnerId(int businessPartnerId){
         ArrayList<Product> products = new ArrayList<>();
-        ArrayList<Integer> productsIds = new ArrayList<>();
         Cursor c = null;
         try {
             c = mContext.getContentResolver().query(DataBaseContentProvider.INTERNAL_DB_URI.buildUpon()
                     .appendQueryParameter(DataBaseContentProvider.KEY_USER_ID, mUser.getUserId())
                     .build(), null,
-                    "SELECT PRS.PRODUCT_ID " +
+                    "SELECT DISTINCT P.PRODUCT_ID, P.NAME, PI.FILE_NAME, B.BRAND_ID, " +
+                            " B.NAME, B.DESCRIPTION, S.CATEGORY_ID, S.SUBCATEGORY_ID, S.NAME, " +
+                            " S.DESCRIPTION, PA.AVAILABILITY, PR.RATING, PA.PRICE, OL.PRODUCT_ID, P.INTERNAL_CODE " +
                     " FROM PRODUCT_RECENTLY_SEEN PRS " +
                         " INNER JOIN PRODUCT P ON P.PRODUCT_ID = PRS.PRODUCT_ID AND P.IS_ACTIVE = ? " +
+                        " INNER JOIN BRAND B ON B.BRAND_ID = P.BRAND_ID AND B.IS_ACTIVE = ? " +
+                        " INNER JOIN SUBCATEGORY S ON S.SUBCATEGORY_ID = P.SUBCATEGORY_ID AND S.IS_ACTIVE = ? " +
+                        " INNER JOIN CATEGORY C ON C.CATEGORY_ID = S.CATEGORY_ID AND C.IS_ACTIVE = ? " +
+                        " LEFT JOIN PRODUCT_PRICE_AVAILABILITY PA ON PA.PRODUCT_ID = P.PRODUCT_ID AND PA.IS_ACTIVE = ? " +
+                        " LEFT JOIN PRODUCT_IMAGE PI ON PI.PRODUCT_ID = P.PRODUCT_ID AND PI.PRIORITY = ? AND PI.IS_ACTIVE = ? " +
+                        " LEFT JOIN PRODUCT_RATING PR ON PR.PRODUCT_ID = P.PRODUCT_ID AND PR.IS_ACTIVE = ? " +
+                        " LEFT JOIN ECOMMERCE_ORDER_LINE OL ON OL.PRODUCT_ID = P.PRODUCT_ID AND OL.BUSINESS_PARTNER_ID = ? " +
+                            " AND OL.USER_ID = ? AND OL.DOC_TYPE=? AND OL.IS_ACTIVE = ? " +
                     " WHERE PRS.BUSINESS_PARTNER_ID = ? AND PRS.USER_ID = ? " +
                     " ORDER BY PRS.PRODUCT_RECENTLY_SEEN_ID desc " +
                     " LIMIT 30",
-                    new String[]{"Y", String.valueOf(businessPartnerId), String.valueOf(mUser.getServerUserId())}, null);
+                    new String[]{"Y", "Y", "Y", "Y", "Y", "1", "Y", "Y",
+                            String.valueOf(Utils.getAppCurrentBusinessPartnerId(mContext, mUser)),
+                            String.valueOf(mUser.getServerUserId()), OrderLineDB.WISH_LIST_DOC_TYPE,
+                            "Y", String.valueOf(businessPartnerId), String.valueOf(mUser.getServerUserId())}, null);
             if(c!=null){
                 while(c.moveToNext()){
-                    productsIds.add(c.getInt(0));
+                    Product product = new Product();
+                    ProductDB.fillLightProductInfoFromCursor(product, c);
+                    products.add(product);
                 }
             }
         } catch (Exception e) {
@@ -69,13 +84,6 @@ public class ProductRecentlySeenDB {
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
-            }
-        }
-        ProductDB productDB = new ProductDB(mContext, mUser);
-        for (Integer productId : productsIds) {
-            Product product = productDB.getProductById(productId);
-            if(product!=null){
-                products.add(product);
             }
         }
         return products;
--- app/src/main/java/com/smartbuilders/smartsales/ecommerce/data/RecommendedProductDB.java
+++ app/src/main/java/com/smartbuilders/smartsales/ecommerce/data/RecommendedProductDB.java
@@ -3,6 +3,7 @@ package com.smartbuilders.smartsales.ecommerce.data;
 import android.content.Context;
 import android.database.Cursor;
 
+import com.smartbuilders.smartsales.ecommerce.utils.Utils;
 import com.smartbuilders.synchronizer.ids.model.User;
 import com.smartbuilders.synchronizer.ids.providers.DataBaseContentProvider;
 import com.smartbuilders.smartsales.ecommerce.model.Product;
@@ -24,21 +25,35 @@ public class RecommendedProductDB {
 
     public ArrayList<Product> getRecommendedProductsByBusinessPartnerId(int businessPartnerId){
         ArrayList<Product> products = new ArrayList<>();
-        ArrayList<Integer> productsIds = new ArrayList<>();
         Cursor c = null;
         try {
             c = mContext.getContentResolver().query(DataBaseContentProvider.INTERNAL_DB_URI.buildUpon()
                     .appendQueryParameter(DataBaseContentProvider.KEY_USER_ID, mUser.getUserId())
                     .build(), null,
-                    "SELECT RP.PRODUCT_ID " +
+                    "SELECT DISTINCT P.PRODUCT_ID, P.NAME, PI.FILE_NAME, B.BRAND_ID, " +
+                            " B.NAME, B.DESCRIPTION, S.CATEGORY_ID, S.SUBCATEGORY_ID, S.NAME, " +
+                            " S.DESCRIPTION, PA.AVAILABILITY, PR.RATING, PA.PRICE, OL.PRODUCT_ID, P.INTERNAL_CODE " +
                     " FROM  RECOMMENDED_PRODUCT RP " +
                         " INNER JOIN PRODUCT P ON P.PRODUCT_ID = RP.PRODUCT_ID AND P.IS_ACTIVE = ? " +
+                        " INNER JOIN BRAND B ON B.BRAND_ID = P.BRAND_ID AND B.IS_ACTIVE = ? " +
+                        " INNER JOIN SUBCATEGORY S ON S.SUBCATEGORY_ID = P.SUBCATEGORY_ID AND S.IS_ACTIVE = ? " +
+                        " INNER JOIN CATEGORY C ON C.CATEGORY_ID = S.CATEGORY_ID AND C.IS_ACTIVE = ? " +
+                        " LEFT JOIN PRODUCT_PRICE_AVAILABILITY PA ON PA.PRODUCT_ID = P.PRODUCT_ID AND PA.IS_ACTIVE = ? " +
+                        " LEFT JOIN PRODUCT_IMAGE PI ON PI.PRODUCT_ID = P.PRODUCT_ID AND PI.PRIORITY = ? AND PI.IS_ACTIVE = ? " +
+                        " LEFT JOIN PRODUCT_RATING PR ON PR.PRODUCT_ID = P.PRODUCT_ID AND PR.IS_ACTIVE = ? " +
+                        " LEFT JOIN ECOMMERCE_ORDER_LINE OL ON OL.PRODUCT_ID = P.PRODUCT_ID AND OL.BUSINESS_PARTNER_ID = ? " +
+                            " AND OL.USER_ID = ? AND OL.DOC_TYPE=? AND OL.IS_ACTIVE = ? " +
                     " WHERE RP.BUSINESS_PARTNER_ID = ? AND RP.IS_ACTIVE = ? " +
                     " ORDER BY RP.PRIORITY desc",
-                    new String[]{"Y", String.valueOf(businessPartnerId), "Y"}, null);
+                    new String[]{"Y", "Y", "Y", "Y", "Y", "1", "Y", "Y",
+                            String.valueOf(Utils.getAppCurrentBusinessPartnerId(mContext, mUser)),
+                            String.valueOf(mUser.getServerUserId()), OrderLineDB.WISH_LIST_DOC_TYPE,
+                            "Y", String.valueOf(businessPartnerId), "Y"}, null);
             if (c!=null) {
                 while(c.moveToNext()){
-                    productsIds.add(c.getInt(0));
+                    Product product = new Product();
+                    ProductDB.fillLightProductInfoFromCursor(product, c);
+                    products.add(product);
                 }
             }
         } catch (Exception e) {
@@ -50,13 +65,6 @@ public class RecommendedProductDB {
                 } catch (Exception e){
                     e.printStackTrace();
                 }
-            }
-        }
-        ProductDB productDB = new ProductDB(mContext, mUser);
-        for (Integer productId : productsIds) {
-            Product product = productDB.getProductById(productId);
-            if(product!=null){
-                products.add(product);
             }
         }
         return products;
