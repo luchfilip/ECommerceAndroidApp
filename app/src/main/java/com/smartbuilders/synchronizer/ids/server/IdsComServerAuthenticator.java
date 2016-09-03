7e11c48ac32217326763fdb959b8a47284b705ef
Alberto Sarco <sarco.jesus@gmail.com>
2016-09-03 19:57:35 -0400

    commit

--- app/src/main/java/com/smartbuilders/synchronizer/ids/server/IdsComServerAuthenticator.java
+++ app/src/main/java/com/smartbuilders/synchronizer/ids/server/IdsComServerAuthenticator.java
@@ -18,7 +18,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.text.TextUtils;
-import android.util.Log;
 
 /**
  * Handles the communication with IDS server
@@ -31,9 +30,7 @@ public class IdsComServerAuthenticator implements ServerAuthenticate {
 
     @Override
     public void userSignUp(User user, String authType, Context ctx) throws Exception {
-    	Log.d(TAG, "userSignUp");
-
-		try {
+    	try {
             //Nos registramos en los servidores de GCM
             user.setGcmRegistrationId(InstanceID.getInstance(ctx).getToken(ctx.getString(R.string.gcm_defaultSenderId),
                     GoogleCloudMessaging.INSTANCE_ID_SCOPE, null));
@@ -67,39 +64,26 @@ public class IdsComServerAuthenticator implements ServerAuthenticate {
 								|| exceptionClass.equals(IOException.class.getName()))){
         					throw new IOException(response.getString(response.getColumnIndex("error_message")));
         				}
+                        if(response.getString(response.getColumnIndex("error_message"))!=null){
+                            if(response.getString(response.getColumnIndex("error_message")).equals("AUTHORIZED")){
+                                throw new Exception(ctx.getString(R.string.user_already_registered));
+                            }else if(response.getString(response.getColumnIndex("error_message")).equals("WRONG_PASSWORD")){
+                                throw new Exception(ctx.getString(R.string.user_wrong_password));
+                            }else if(response.getString(response.getColumnIndex("error_message")).equals("NOT_REGISTERED")){
+                                throw new Exception(ctx.getString(R.string.user_not_exist_in_server));
+                            }
+                        }
         				throw new Exception(response.getString(response.getColumnIndex("error_message")));
 					}
 					user.setUserProfileId(Integer.valueOf(response.getString(response.getColumnIndex("userProfileId"))));
 					user.setServerUserId(Integer.valueOf(response.getString(response.getColumnIndex("serverUserId"))));
 					user.setAuthToken(response.getString(response.getColumnIndex("authToken")));
 					user.setSessionToken(ApplicationUtilities.ST_NEW_USER_AUTHORIZED);
-//					switch (response.getInt(response.getColumnIndex("state"))) {
-//						case ApplicationUtilities.NEW_USER_AUTHORIZED:
-//							getGcmRegId = true;
-//							//TODO: colocar aqui el AuthToken unico del usuario
-//							user.setAuthToken(null);
-//							user.setSessionToken(ApplicationUtilities.ST_NEW_USER_AUTHORIZED);
-//						break;
-//						case ApplicationUtilities.USER_AUTHORIZED:
-//							getGcmRegId = true;
-//							user.setSessionToken(ApplicationUtilities.ST_USER_AUTHORIZED);
-//						break;
-//						case ApplicationUtilities.USER_NOT_AUTHORIZED:
-//							user.setSessionToken(ApplicationUtilities.ST_USER_NOT_AUTHORIZED);
-//						break;
-//						case ApplicationUtilities.USER_WRONG_PASSWORD:
-//							user.setSessionToken(ApplicationUtilities.ST_USER_WRONG_PASSWORD);
-//						break;
-//						case ApplicationUtilities.USER_NOT_EXIST_IN_SERVER:
-//							user.setSessionToken(ApplicationUtilities.ST_USER_NOT_EXIST_IN_SERVER);
-//						break;
-//					}
 				}else{
 					user.setSessionToken(ctx.getString(R.string.user_response_move_to_next_false));
 				}
 			}
         }catch(Exception e){
-        	e.printStackTrace();
         	throw e;
         }finally{
         	if(response!=null){
@@ -110,7 +94,6 @@ public class IdsComServerAuthenticator implements ServerAuthenticate {
     
     @Override
     public void userSignIn(User user, String authType, Context ctx) throws Exception {
-        Log.d(TAG, "userSignIn");
         boolean getGcmRegId = false;
         Cursor response = null;
         try{
@@ -163,7 +146,6 @@ public class IdsComServerAuthenticator implements ServerAuthenticate {
 				}
 			}
         }catch(Exception e){
-        	e.printStackTrace();
         	throw e;
         }finally{
         	if(response!=null){
@@ -182,8 +164,6 @@ public class IdsComServerAuthenticator implements ServerAuthenticate {
 
     @Override
     public void userGetAuthToken(User user, String authType, Context ctx) throws Exception {
-        Log.d(TAG, "userGetAuthToken");
-        boolean getGcmRegId = false;
         Cursor response = null;
         try{
             response = ctx.getContentResolver()
@@ -191,7 +171,7 @@ public class IdsComServerAuthenticator implements ServerAuthenticate {
                                     .appendQueryParameter(SynchronizerContentProvider.KEY_USER_ID, user.getUserId()).build(),
                             null, null, null, null);
             if(response == null){
-                user.setSessionToken(ctx.getString(R.string.error_validating_user_return_null));
+                throw new Exception(ctx.getString(R.string.error_validating_user_return_null));
             }else{
                 if(response.moveToNext()){
                     //Se revisa si hubo alguna excepcion al ejecutar la consulta
@@ -205,40 +185,32 @@ public class IdsComServerAuthenticator implements ServerAuthenticate {
                                         || exceptionClass.equals(IOException.class.getName()))){
                             throw new IOException(response.getString(response.getColumnIndex("error_message")));
                         }
+                        if(response.getString(response.getColumnIndex("error_message"))!=null){
+                            if(response.getString(response.getColumnIndex("error_message")).equals("WRONG_PASSWORD")){
+                                throw new Exception(ctx.getString(R.string.user_wrong_password));
+                            }else if(response.getString(response.getColumnIndex("error_message")).equals("NOT_REGISTERED")){
+                                throw new Exception(ctx.getString(R.string.user_not_exist_in_server));
+                            }else if(response.getString(response.getColumnIndex("error_message")).equals("DISABLED")){
+                                throw new Exception(ctx.getString(R.string.user_disabled));
+                            }
+                        }
                         throw new Exception(response.getString(response.getColumnIndex("error_message")));
                     }
                     user.setAuthToken(response.getString(response.getColumnIndex("authToken")));
-                    if(user.getAuthToken()!=null){
-                        getGcmRegId=true;
-                        //TODO: leer este valor del servidor en la tabla IDS_USER
-                        user.setSessionToken(ApplicationUtilities.ST_USER_AUTHORIZED);
-                    }
-                }else{
-                    user.setSessionToken(ctx.getString(R.string.user_response_move_to_next_false));
                 }
             }
         }catch(Exception e){
-            e.printStackTrace();
             throw e;
         }finally{
             if(response!=null){
                 response.close();
             }
         }
-        if(getGcmRegId){
-            //Nos registramos en los servidores de GCM
-            ctx.startService(new Intent(ctx, RegistrationIntentService.class)
-                    .putExtra(RegistrationIntentService.KEY_USER_ID, user.getUserId()));
-        }
-        if (TextUtils.isEmpty(user.getSessionToken())) {
-            user.setSessionToken(ctx.getString(R.string.user_session_token_null));
-        }
     }
 
     @Override
 	public void userSignOut(User user, String syncState, Context ctx)
 			throws Exception {
-    	Log.d(TAG, "userSignOut");
         Cursor response = null;
         try{
         	response = ctx.getContentResolver()
